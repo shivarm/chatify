4c3cf8ff75f3a66dc70b1f7f5561b3c93ea854b2
Shivam Sharma <meshivam81@gmail.com>
2025-09-18 07:52:36 +0530

    sevalla: ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "node:path";
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 
@@ -9,6 +11,7 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 5001;
 
@@ -18,6 +21,14 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.json(__dirname, "../client/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
