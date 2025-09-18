7438dd94f51bf69b22e8617c28f1ea6805c97df5
Shivam Sharma <meshivam81@gmail.com>
2025-09-18 08:09:32 +0530

    fix: sevalla require node.js 20+

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,14 +23,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.json(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log("Server running on port: " + PORT);
   connectDB();
 });
