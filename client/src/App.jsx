68c37aff5c27a287c89f369a209d22559a490abe
Shivam Sharma <meshivam81@gmail.com>
2025-09-19 08:10:28 +0530

    chore: set axios instance and page route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router";
+
+import Layout from "./layout/Layout";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
