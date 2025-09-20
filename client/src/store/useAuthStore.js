cde9ecfe1b196bfbf030d30bf06a54ba18f4ca8b
Shivam Sharma <meshivam81@gmail.com>
2025-09-20 07:30:58 +0530

    feat(ui): add signup page, check auth user with zustand (#8)
    * fix: avoid log
    * fix: some type and error

---
+++ client/src/store/useAuthStore.js
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
+
+  checkAuth: async () => {
+    try {
+      const response = await axiosInstance.get("/auth/check");
+      set({ authUser: response.data });
+    } catch (error) {
+      console.log("Error in authCheck", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signUp: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const response = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: response.data });
+      toast.success("Account created successfully");
+    } catch (error) {
+      toast.error(error.response?.data?.message || "An error occurred during sign up.");
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+}));
