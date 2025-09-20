e2a9cdf4f8e15596924cc1f58c281aee15eef5d0
Shivam Sharma <meshivam81@gmail.com>
2025-09-20 07:22:32 +0530

    feat(ui): add signup page, check auth user with zustand

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,36 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router";
+import { useEffect } from "react";
 
 import Layout from "./layout/Layout";
 import ChatPage from "./pages/ChatPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import PageLoader from "./components/PageLoader";
+
+import { useAuthStore } from "./store/useAuthStore";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { checkAuth, isChecking, authUser } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log("auth user", authUser);
+
+  if (isChecking) {
+    return <PageLoader /> 
+  }
+
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
       </Routes>
+      <Toaster />
     </Layout>
   );
 }
