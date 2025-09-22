1dcc8012996d482d9cce3ac623ccb4ddf0243d27
Shivam Sharma <meshivam81@gmail.com>
2025-09-22 12:20:23 +0530

    chore(ui): complete sidebar component with chats and contacts list

--- client/src/components/ChatsList .jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ChatsList () {
-  return (
-    <div>ChatsList </div>
-  )
-}
-
-export default ChatsList 
--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -3,7 +3,7 @@ import { useChatStore } from "../store/useChatStore";
 import AnimatedBorder from "../components/AnimatedBorder";
 import ProfileHeader from "../components/ProfileHeader";
 import ActiveTabSwitch from "../components/ActiveTabSwitch";
-import ChatsList from "../components/ChatsList ";
+import ChatsList from "../components/ChatsList";
 import ContactList from "../components/ContactList";
 import ChatContainer from "../components/ChatContainer";
 import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
