57e750c20f8b13f980b037d1111f9b59e94d27c4
Shivam Sharma <meshivam81@gmail.com>
2025-09-23 09:34:26 +0530

    feat(ui): fetch user chat messages

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,12 +1,15 @@
-import { useEffect } from 'react';
-import ChatHeader from './ChatHeader';
+import { useEffect } from "react";
+import ChatHeader from "./ChatHeader";
 
-import { useAuthStore } from '../store/useAuthStore';
-import { useChatStore } from '../store/useChatStore';
+import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
 
+import MessagesLoadingSkeloton from "./MessagesLoadingSkeleton";
+import NoChatHistoryPlaceHolder from "./NoChatHistoryPlaceholder";
+import MessageInput from "./MessageInput";
 
 function ChatContainer() {
-  const { selectedUser, getMessagesByUserId } = useChatStore();
+  const { selectedUser, messages, getMessagesByUserId, isMessagesLoading } = useChatStore();
   const { authUser } = useAuthStore();
 
   useEffect(() => {
@@ -20,7 +23,42 @@ function ChatContainer() {
   return (
     <>
       <ChatHeader />
-       
+      <div className="flex-1 overflow-y-auto px-6 py-8">
+        {messages.length > 0 && !isMessagesLoading ? (
+          <div className="max-w-3xl mx-auto space-y-6">
+            {messages.map((msg) => (
+              <div
+                key={msg._id}
+                className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+              >
+                <div
+                  className={`chat-bubble relative ${
+                    msg.senderId === authUser._id
+                      ? "bg-cyan-500 text-white"
+                      : "bg-slate-800 text-slate-200"
+                  }`}
+                >
+                  {msg.image && (
+                    <img src={msg.image} alt="shared" className="rounded-lg h-48 object-cover" />
+                  )}
+                  {msg.text && <p className="mt-2">{msg.text}</p>}
+                  <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
+                    {new Date(msg.createdAt).toLocaleTimeString(undefined, {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : isMessagesLoading ? (
+          <MessagesLoadingSkeloton />
+        ) : (
+          <NoChatHistoryPlaceHolder name={selectedUser.fullName} />
+        )}
+      </div>
+      <MessageInput />
     </>
   );
 }
---
+++ client/src/components/MessageInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function MessageInput() {
+  return (
+    // Todo: comple message input functionality
+    <div>MessageInput</div>
+  )
+}
+
+export default MessageInput
