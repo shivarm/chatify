65f8c3da4cca8438e22ffc91a6bcdb610046a523
Shivam Sharma <meshivam81@gmail.com>
2025-09-23 17:29:37 +0530

    fix: remove duplicate file

--- client/public/useKeyboardSound.js
+++
@@ -1,6 +0,0 @@
-const keyStrokeSound = [
-  new Audio("/sounds/keystroke1.mp3"),
-  new Audio("/sounds/keystroke2.mp3"),
-  new Audio("/sounds/keystroke3.mp3"),
-  new Audio("/sounds/keystroke4.mp3"),
-];
